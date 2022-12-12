43db62ff37a72745f438f9458a0dadf69fd3c334
Kane <LisaxKane@gmail.com>
2022-12-11 23:18:15 -0600

    fixed

--- app.js
+++ app.js
@@ -1,36 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bodyParser = require('body-parser')
-require('dotenv/config');
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv/config");
 
-
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 //import Routes
-const postsRoute = require('./routes/post');
+const postsRoute = require("./routes/post");
 
-app.use('/posts', postsRoute)
-
-
+app.use("/posts", postsRoute);
 
 //ROUTES
 
-
-
-   
-
-
-//connect to DB 
+//connect to DB
 mongoose.connect(process.env.DB_CONNECTION, () => {
+  console.log("connected to DB");
+});
 
-        console.log('connected to DB')
-    });
-    
-
-const Port = 8080
-app.listen(Port, ()=> {
-    console.log(`it's alive! now listening to port ${Port}`)
+const Port = 3000;
+app.listen(Port, () => {
+  console.log(`it's alive! now listening to port ${Port}`);
 });
